884b8144010df2a6c02159b60b996ae17e6cd750
manishpatil045 <manishspatil2405>
2025-09-10 07:07:51 +0530

    second commit

--- RideSharing.cpp
+++ RideSharing.cpp
@@ -19,25 +19,29 @@ class passanger{
     int age;
     int rides;
 };
-void driver_login(){
+void login(int x){
+    if(x==1){
     driver *p=new driver();
-        cout<<"1.name"; cin>>p->driv.name;
-        cout<<"2.mobile number"; cin>>p->driv.mobile;
-        cout<<"3.vehicle"; cin>>p->vehicle;
-        cout<<"4.experience(years)"; cin>>p->experience;
-}
-void rider_login(){
+    cout<<"1.name"; cin>>p->driv.name;
+    cout<<"2.mobile number"; cin>>p->driv.mobile;
+    cout<<"3.vehicle"; cin>>p->vehicle;
+    cout<<"4.experience(years)"; cin>>p->experience;
+    check_my_rides(); 
+    }
+    else{
     passanger *r=new passanger();
-        cout<<"1.name:"; cin>>r->pass.name;
-        cout<<"2.mobile number"; cin>>r->pass.mobile;
-        cout<<"3.age"; cin>>r->age;
-        cout<<"4.rides"; cin>>r->rides;
+    cout<<"1.name:"; cin>>r->pass.name;
+    cout<<"2.mobile number"; cin>>r->pass.mobile;
+    cout<<"3.age"; cin>>r->age;
+    cout<<"4.no of passanger"; cin>>r->rides;
+    book_ride();
+    check_my_rides();
+    }
 }
 int main(){
-    int i;
     cout<<"login\n";
     int n;
     cout<<"1.Driver\n2.Passanger:\n";
     cin>>n;
-    (n==1)?driver_login():rider_login();   
+    login(n);   
 }
